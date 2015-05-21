9b2b9e2679f6bc8055805112cdf40c5228737bb8
DBrubaker71 <DaveBrubaker@hotmail.com>
2015-05-21 13:46:11 -0400

    17. Installing and Utilizing jQuery Plugins

--- script/script17.js
+++ script/script17.js
@@ -2,7 +2,15 @@
 $(function() {
 	
 	//	Initialization Code
+	//	Init Tabs
 	$('#tabs').tabs();
+	
+	//	Init DatePicker
+	$('#datepicker').datepicker({ 
+		onSelect: function(dateText, inst) {
+			$('#title').text('You picked: ' + dateText);	
+		}
+	});
 	
 	
 });
